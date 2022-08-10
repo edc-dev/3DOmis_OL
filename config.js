3c90dc8c6e93de657762fd7f63f3e6730653ab79
tsimenc <tomislav.simenc@edc.hr>
2022-08-10 12:04:43 +0200

    ver. 0.1

---
+++ config.js
@@ -0,0 +1,38 @@
+
+
+//github
+const dataSources= {
+mjernaZona : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/dma.geojson",
+vodospremnik : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/tank.geojson",
+pumpa : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/pump.geojson",
+vod : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/pipe.geojson",
+prikljucak : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/meter.geojson",
+mjernoMjesto : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/gauge.geojson",
+telemetrija : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/telemetry.geojson",
+podrucjeZgrada : "https://raw.githubusercontent.com/edc-dev/example-geojson/main/podrucjeZgrada.geojson"
+};
+
+
+/*
+const request = require('request');
+
+const URL = 'https://raw.githubusercontent.com/myuser/myrepo/master/myfile.js';
+const TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+var options = {
+  url: URL,
+  headers: {
+    'Authorization': 'token ' + TOKEN
+  }
+};
+
+function callback(error, response, body) {
+      console.log(response.statusCode);
+      console.error(error);
+      console.log(body);
+}
+
+request(options, callback);
+
+*/
+
